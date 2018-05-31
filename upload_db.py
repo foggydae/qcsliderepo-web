43881920933402cfd4e46daea8c4f4390bb89f09
ren.zuo <foggydew3@gmail.com>
2018-05-31 19:17:03 -0400

    update db uploading module.

--- upload_db.py
+++ upload_db.py
@@ -14,16 +14,16 @@ thumb_path = "./FlaskApp/static/data/thumbnail/"
 slide_creation_date = datetime.date.today().strftime("%m/%d/%Y")
 
 
-# db_name = 'histoqc'
-# db_ip = 'localhost'
-# db_port = 3306
-# db_user = 'root'
-# db_password = 'histoqcweb'
-# db_table = 'file'
+db_name = 'histoqc'
+db_ip = 'localhost'
+db_port = 3306
+db_user = 'root'
+db_password = 'histoqcweb'
+db_table = 'file'
 
-# db = MySQLdb.connect(host=db_ip, port=db_port, user=db_user, passwd=db_password, db=db_name)
-# cursor = db.cursor()
-# db.set_character_set('utf8')
+db = MySQLdb.connect(host=db_ip, port=db_port, user=db_user, passwd=db_password, db=db_name)
+cursor = db.cursor()
+db.set_character_set('utf8')
 
 
 file_list = pd.read_csv('./histoqc_upload.txt', sep='\t', header=0)
@@ -38,47 +38,47 @@ for i, row in file_list.iterrows():
     command = "("
     thumb_file = '%s%s' % (os.path.splitext(cur_file)[0], '.png')
     
-    # comments = row["comment"]
-    # result = re.search(r'Date = ([0-9]{2}/[0-9]{2}/)([0-9]{2})', comments)
-    # if result:
-    #     cur_creation_date = result.group(1) + "20" + result.group(2)
-    # else:
-    #     cur_creation_date = slide_creation_date
+    comments = row["comment"]
+    result = re.search(r'Date = ([0-9]{2}/[0-9]{2}/)([0-9]{2})', comments)
+    if result:
+        cur_creation_date = result.group(1) + "20" + result.group(2)
+    else:
+        cur_creation_date = slide_creation_date
         
-    # attributes.append(thumb_file)
-    # attributes.append(cur_file)
-    # attributes.append(datetime.date.today().strftime("%m/%d/%Y"))
-    # attributes.append("axj232@case.edu")
-    # attributes.append("Breast")
-    # attributes.append(cur_creation_date)
-    # attributes.append(row["Magnification"])
-    # attributes.append(row["comments"])
-    # attributes.append("H&E")
-    # attributes.append(row["comment"])
-    # attributes.append(str(row["width"])+'x'+str(row["height"]))
-    # attributes.append(round(os.path.getsize(data_path + cur_file) / float(1024 * 1024 * 1024), 2))
-    # attributes.append(os.path.splitext(cur_file)[1])
-    # attributes.append("Aperio")
-    # attributes.append("FFPE")
-    # attributes.append("Resection")
+    attributes.append(thumb_file)
+    attributes.append(cur_file)
+    attributes.append(datetime.date.today().strftime("%m/%d/%Y"))
+    attributes.append("axj232@case.edu")
+    attributes.append("Breast")
+    attributes.append(cur_creation_date)
+    attributes.append(row["Magnification"])
+    attributes.append(row["comments"])
+    attributes.append("H&E")
+    attributes.append(row["comment"])
+    attributes.append(str(row["width"])+'x'+str(row["height"]))
+    attributes.append(round(os.path.getsize(data_path + cur_file) / float(1024 * 1024 * 1024), 2))
+    attributes.append(os.path.splitext(cur_file)[1])
+    attributes.append("Aperio")
+    attributes.append("FFPE")
+    attributes.append("Resection")
     
-    # for j, attribute_val in enumerate(attributes):
-    #     command += "'" + str(attribute_val) + "'"
-    #     if j == len(attributes) - 1:
-    #         command += ")"
-    #     else:
-    #         command += ","
+    for j, attribute_val in enumerate(attributes):
+        command += "'" + str(attribute_val) + "'"
+        if j == len(attributes) - 1:
+            command += ")"
+        else:
+            command += ","
     
-    # db_command += command
-    # if i == len(file_list) - 1:
-    #     db_command += ";"
-    # else:
-    #     db_command += ","
+    db_command += command
+    if i == len(file_list) - 1:
+        db_command += ";"
+    else:
+        db_command += ","
 
     fname = data_path + cur_file
     osh = openslide.OpenSlide(fname)
     thumb = np.array(osh.get_thumbnail((100,100)))
     io.imsave(thumb_path + thumb_file, thumb)
 
-# cursor.execute(db_command)
-# db.commit()
+cursor.execute(db_command)
+db.commit()
